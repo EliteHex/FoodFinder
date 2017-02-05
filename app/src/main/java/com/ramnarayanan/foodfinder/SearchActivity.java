373eda48971e2fb37e59344bfc887718f93f0a59
Ram <ramn0711@gmail.com>
2017-02-05 01:51:03 -0500

    Added toolbar icons to Map Activity. Next step is to define Search Activity.
    Updated permissions issue, now handles request using onRequestPermissionsResult

---
+++ app/src/main/java/com/ramnarayanan/foodfinder/SearchActivity.java
@@ -0,0 +1,29 @@
+package com.ramnarayanan.foodfinder;
+
+import android.app.SearchManager;
+import android.content.Context;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.Menu;
+
+public class SearchActivity extends AppCompatActivity {
+    private static final String TAG = "SearchActivity";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_search);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        Log.d(TAG, "onCreateOptionsMenu: starts");
+
+        getMenuInflater().inflate(R.menu.menu_search,menu);
+
+        SearchManager searchManager = (SearchManager) getSystemService(Context.SEARCH_SERVICE);
+
+        return true;
+    }
+}
